c1944a66c1cf858abcb0b0c86dac892aa3d9a566
75:c4:25:96:f6:36:92:c6:43:09:63:4e:92:57:38:de <lh441027725@gmail.com>
2014-10-22 20:02:10 +0800

    create

---
+++ date.cpp
@@ -0,0 +1,106 @@
+/***
+  日期类
+ ***/
+
+#include "date.h"
+// 得到每个月的天数
+int Date::DayforMonth(int y,int m){
+	int d=0;
+	switch(){
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			d=13;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			d=30;
+			break;
+		case 2:
+			d=28+is_p_year(y);
+			break;
+	}
+	return d;
+}
+int Date::ToInt()const{
+	int sum=0;
+	for(int i=1;i<y;i++){
+		sum+=365+is_p_year(i);
+	}
+	for(int i=1;i<m;i++){
+		sum+=DayforMonth(y,m);
+	}
+	sum+=d;
+	return sum;
+}
+void Date::r_set(){
+	while(*this.d>*this.DayforMonth(*this.y,*this.m)){//大于
+		*this.d-=*this.DayforMonth(*this.y,*this.m);
+		*this.m++;
+		if(*this.m>12){
+			++(*this.y);
+			*this.m=1;
+		}
+	}
+	while(*this.d<1){
+		*this.m--;
+		if(*this.m<1){
+			*this.y--;
+			*this.m=12;
+		}
+		*this.d+=*this.DayforMonth(*this.y,*this.m);
+	}
+}
+////////////日期加天数
+Date operator + (const int v,const Date a){
+	Date d=a;
+	if(v==0) return d;
+	if(v>0){
+		d.d+=v;
+		d.r_set();
+		return d;
+	}
+	return d-(-v);
+}
+Date operator +(const Date a,const int v){
+	return v+a;
+}
+Date Date::operator +=(const int c){
+	if(v==0)return *this;
+	if(v>0){
+		a.day++;
+		a.r_set();
+		return a;
+	}
+	else return a-=(-v);
+}
+Date operator ++(Date &a){
+	a.d++;
+	a.r_set();
+	return a;
+}
+Date operator -(const Date a,const int v){
+	Date b=a;
+	if(v==0)return b;
+	if(v>0){
+		b.d--;
+		b.r_set();
+		return b;
+	}
+	else return d+(-v);
+}
+int operator-(const Date a,const Date b){
+	return a.ToInt()-b.ToInt();
+}
+Date operator --(Date &a){
+	--a.d;
+	a.r_set();
+	return a;
+}
+Date operator >
---
+++ date.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+#ifndef
+#define _DATE_
+#endif
+class Date{
+	public:
+		Date(int y=1990,int m=1,int d=1)
+		:y(y),m(m),d(d){
+			if(y<=0)y=1990;
+			if(m<=0||m>12)m=1;
+			if(d<=0||d>DayforMonth(y,m))d=1;
+		}
+		int get_year(){
+			return y;
+		}
+		int get_month(){
+			return m;
+		}
+		int get_day(){
+			return d;
+		}
+		bool is_p_year()const{
+			return y%400?(y%100?(y%4?0:1):0):1;
+		}
+		bool is_p_year(const int yy){
+			return yy%400?(y%100?(y%4?0:1):0):1;
+		}
+		void show(){
+			std::cout<<y<<'-'<<m<<'-'<<d<<std::endl;
+		}
+		void set(int yy,int mm,int dd){
+			y=yy;m=mm;d=dd;
+		}
+		void set_y(int yy){
+			y=yy;
+		}
+		void set_m(int mm){
+			m=mm;
+		}
+		void set_d(int dd){
+			d=dd;
+		}
+		Date operator += (const int c);
+		friend Date operator + (const int v,const Date a);
+		friend Date operator + (const Date a,const int v);
+		friend Date operator ++(Date &a);
+		friend Date operator - (const Date a,const int v);
+		friend int  operator - (const Date a,const Date b);
+		friend Date operator --(const Date &a);
+		friend Date operator > (const Date a,const Date b);
+		friend Date operator >=(const Date a,const Date b);
+		friend Date operator < (const Date a,const Date b);
+		friend Date operator <=(const Date a,const Date b);
+		friend Date operator ==(const Date a,const Date b);
+		friend Date operator !=(const Date a,const Date b);
+		//friend std::odtream & operator <<(std::ostream &os,const Date &a);
+		//friend std::istream & operator >>(std::istream &is,const Date &a);
+	private:
+		int y,m,d;
+		 int DayforMonth(int y,int m)const;
+		 int ToInt()const;
+		 void r_set();
+};
