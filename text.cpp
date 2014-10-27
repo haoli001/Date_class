cf8cc8883fb59498efb901947e9e5e9ac684b917
75:c4:25:96:f6:36:92:c6:43:09:63:4e:92:57:38:de <lh441027725@gmail.com>
2014-10-27 19:22:13 +0800

    Revert 82de945..f62c3a8
    This rolls back to commit 82de945012b039a1254f24489805a185ff83893c.

--- text.cpp
+++ text.cpp
@@ -7,6 +7,14 @@ int main(){
 	b.set(2012,2,29);
 	a.show();
 	b.show();
+	cout<<a.get_year()<<endl;
+	cout<<b.get_month()<<endl;
+	a.set_m(13);
+	a.set_d(40);
+	a.show();
+	b.set_d(-1);
+	b.show();
+
 	c[0].show();
 	c[0].set_y(2003);
 	c[0].show();
